f5873f5ff17da32b0bf1232da3ac40d2552c0924
banerjri <banerjri@mail.uc.edu>
2023-06-19 23:19:34 -0500

    complted

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,67 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+@SuppressWarnings("ALL")
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double GallonsinTank = 0.0;
+        double FuelEfficiency = 0.0;
+        double GasPrice = 0.0;
+        double Costper100Mile;
+        double distance;
+        String Trash = "";
+        boolean done = false;
+        do
+            {
+                System.out.print("Enter gallons of gas in the tank : ");
+                if (in.hasNextDouble())
+                {
+                    GallonsinTank = in.nextDouble();
+                    in.nextLine();
+                    done = true;
+                } else
+                {
+                    Trash = in.nextLine();
+                    System.out.println("\nYou said gallons in the tank was : " + Trash);
+                    System.out.println("Enter a valid number");
+                }
+
+            } while (!done) ;
+            do {
+                System.out.print("Enter the Miles per Gallon : ");
+                if (in.hasNextDouble())
+                {
+                    FuelEfficiency = in.nextDouble();
+                    in.nextLine();
+                    done = true;
+                } else
+                {
+                    Trash = in.nextLine();
+                    System.out.println("\nYou said Miles per gallon was : " + Trash);
+                    System.out.println("Enter a valid number");
+                }
+            } while (!done);
+            do
+            {
+                System.out.print("Enter the gas Price : ");
+                if (in.hasNextDouble())
+                {
+                    GasPrice = in.nextDouble();
+                    in.nextLine();
+                    done = true;
+                }
+                else
+                {
+                    Trash = in.nextLine();
+                    System.out.println("\nYou said price of gas was : " + Trash);
+                    System.out.println("Enter a valid number");
+                }
+            } while (!done);
+        Costper100Mile = (100 / FuelEfficiency) * GasPrice;
+        distance = GallonsinTank * FuelEfficiency;
+        System.out.println("The cost per 100 miles " + Costper100Mile);
+        System.out.println("The distance covered in full tank " + distance);
     }
 }
